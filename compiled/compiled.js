16b97bf1e39cc751a6d0261c442a3abfeb3aa457
GREY-Studio <legofgoat@gmail.com>
2016-07-25 19:07:18 +0200

    Refactoring three.js init

--- compiled/compiled.js
+++ compiled/compiled.js
@@ -158,32 +158,20 @@ define(function() {
 
 define(['jquery', 'three'], function($, THREE) {
 
-  var mouseX = 0, mouseY = 0;
+  var mouseX = 0,
+      mouseY = 0,
+      scene = new THREE.Scene(),
+      renderer = new THREE.WebGLRenderer(),
+      camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, .1, 1000),
+      axis = new THREE.AxisHelper(30);
 
-  var scene = new THREE.Scene();
-
-  var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, .1, 1000);
   camera.position.set( 0, 0, 400 );
-
-  var renderer = new THREE.WebGLRenderer();
-
   renderer.setPixelRatio( window.devicePixelRatio );
-
-  size();
-  $(window).on('resize', size);
-  $(document).on('mousemove', onDocumentMouseMove);
-
-  function size(e) {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-
-  var axis = new THREE.AxisHelper(30);
-
   scene.add(axis);
 
-  var cubeGEO = new THREE.BoxGeometry(20,20,20);
-  var cubeMAT = new THREE.MeshBasicMaterial({color:0xdddddd, wireframe:true});
-  var cube = new THREE.Mesh(cubeGEO, cubeMAT);
+  var cubeGEO = new THREE.BoxGeometry(20,20,20),
+      cubeMAT = new THREE.MeshBasicMaterial({color:0xdddddd, wireframe:true}),
+      cube = new THREE.Mesh(cubeGEO, cubeMAT);
 
   cube.position.x = 0;
   cube.position.y = 0;
@@ -191,22 +179,21 @@ define(['jquery', 'three'], function($, THREE) {
 
   scene.add(cube);
 
-
-  //camera.lookAt(scene.position);
-
   $('.WebGL').append(renderer.domElement);
 
-  function onDocumentMouseMove( event ) {
+  function onDocumentMouseMove(e) {
+		var windowHalfX = window.innerWidth / 2,
+        windowHalfY = window.innerHeight / 2;
 
-		var windowHalfX = window.innerWidth / 2;
-		var windowHalfY = window.innerHeight / 2;
-		mouseX = ( event.clientX - windowHalfX ) / 2;
+  	mouseX = ( event.clientX - windowHalfX ) / 2;
 		mouseY = ( event.clientY - windowHalfY ) / 2;
-
 	}
 
-  function animate() {
+  function size(e) {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
+  function animate() {
 		requestAnimationFrame( animate );
 
 		camera.position.x += ( mouseX - camera.position.x ) * .05;
@@ -214,13 +201,14 @@ define(['jquery', 'three'], function($, THREE) {
 		camera.lookAt( scene.position );
 
 		renderer.render( scene, camera );
-
 	}
 
+  size();
   animate();
 
-
-
+  $(window).on('resize', size);
+  $(document).on('mousemove', onDocumentMouseMove);
+  
 });
 
 
